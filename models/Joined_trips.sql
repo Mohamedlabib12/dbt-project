04de0c8fe2484a300fee64ef76eea6f046df78f1
mohamed labib <mohameddbt2555@gmail.com>
2025-03-24 12:19:19 +0000

    commit

--- models/Joined_trips.sql
+++ models/Joined_trips.sql
@@ -6,17 +6,21 @@ SELECT
     a.*,
     pu.Zone AS pickup_zone,
     pu.Borough AS pickup_borough,
-    pu.service_zone AS pickup_service_zone,
+    pu.service_zone AS pickup_service_zone ,
+    pu.latitude AS pickup_latitute ,
+    pu.longitude AS pickup_longitude ,
     do.Zone AS dropoff_zone,
     do.Borough AS dropoff_borough,
-    do.service_zone AS dropoff_service_zone
+    do.service_zone AS dropoff_service_zone,
+    pu.latitude AS pickup_latitute ,
+    pu.longitude AS pickup_longitude ,
 FROM
     aggregated_data a
 LEFT JOIN
-    {{ ref('taxi_zone_lookup') }} pu
+    {{ ref('taxi_zones_coordinates') }} pu
     ON a.PULocationID = pu.LocationID
 LEFT JOIN
-    {{ ref('taxi_zone_lookup') }} do
+    {{ ref('taxi_zones_coordinates') }} do
     ON a.DOLocationID = do.LocationID
 ORDER BY
     a.pickup_year, a.pickup_month, a.pickup_day
